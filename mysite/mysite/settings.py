188c7536e01753628a97812db85690b6b08f298c
Júlio Amorim <julio.l.amorim@hotmail.com>
2018-09-18 11:16:40 -0300

    Adição de do modelo da aplicação, referenccia para o banco de dados também. Criação das migrations dos novos modelos, e ativação do app 'model' manipulado

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pools.apps.PoolsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
