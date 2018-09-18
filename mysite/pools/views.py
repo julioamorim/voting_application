5f36253c161b0dd8102f6df089b00d161b9fcefa
Júlio Amorim <julio.l.amorim@hotmail.com>
2018-09-18 14:24:37 -0300

    Adição d enovas views

--- mysite/pools/views.py
+++ mysite/pools/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, request_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, request_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, request_id):
+    return HttpResponse("You're voting on question %s." % question_id)
